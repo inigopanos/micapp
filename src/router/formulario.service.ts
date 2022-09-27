ef6a111e9c0cf6bff8ba5139c60628a0804c36c5
Íñigo Paños <inigopanos@gmail.com>
2022-09-28 00:06:44 +0200

    Añadido axios y funciones de enviar formulario, el problema ahora radica en el back

---
+++ src/router/formulario.service.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const FormularioServices = {
+  enviarFormulario,
+};
+
+const FORMULARIO_API = 'http://localhost:4000/formulario';
+
+export function enviarFormulario(formularioPDF: any) {
+  console.log('Pre post formulario');
+  return axios.post(`${FORMULARIO_API}`, formularioPDF, {});
+}
---
+++ src/store/formulario.modules.ts
@@ -0,0 +1,11 @@
+// import router from '@/router';
+// import { FormularioServices } from '@/router/formulario.service';
+
+// const actions = {
+//   sendForm({ dispatch, commit }: { dispatch: any; commit: any }) {
+//     FormularioServices.enviarFormulario().then((contenidoFormulario: any) => {
+//       commit('sendFormSuccess', contenidoFormulario);
+//       dispatch('alert/success', 'Éxito al generar formulario');
+//     });
+//   },
+// };
