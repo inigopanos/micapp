cac0808e814d954aac9d1bf26ed5852e0c1875ee
Iñigo <inigopanos@gmail.com>
2023-03-19 12:49:45 +0100

    Cambiado capacitor.config.ts, añadida configuración para iOS. Cambiado webDir de www a build, no funcionaba porque el emulador de Android se seguía viendo blanco. Este commit lo cambia a index.html a ver si hay suerte.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,37 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'micapp',
-  webDir: 'dist',
-  bundledWebRuntime: false
+let config: CapacitorConfig;
+
+const baseConfig: CapacitorConfig = {
+  appId: 'com.micapp.app',
+  appName: 'Micapp',
+  webDir: 'index.html',
+  bundledWebRuntime: true
 };
+
+switch (process.env.NODE_ENV) {
+  case 'qa':
+    config = {
+      ...baseConfig,
+      ios: {
+        scheme: 'App QA',
+      },
+      android: {
+        flavor: 'qa',
+      },
+    };
+    break;
+  default:
+    config = {
+      ...baseConfig,
+      ios: {
+        scheme: 'App',
+      },
+      android: {
+        flavor: 'dev',
+      },
+    };
+    break;
+}
 
 export default config;
