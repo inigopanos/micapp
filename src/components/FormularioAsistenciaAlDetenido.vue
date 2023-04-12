ea446a908d07c1ae6bdffaedb00ee125f5a66a67
Iñigo <inigopanos@gmail.com>
2023-04-12 20:52:35 +0200

    Añadido bloque para comprobar que se envía el correo

--- src/components/FormularioAsistenciaAlDetenido.vue
+++ src/components/FormularioAsistenciaAlDetenido.vue
@@ -210,6 +210,7 @@
           </div>
         </div>
         <button id="generate" v-on:click="handleSubmit()">Guardar PDF</button>
+        <div id="sent_email">Se ha enviado el correo</div>
       </main>
     </section>
   </div>
@@ -293,6 +294,20 @@ export default defineComponent({
 
       html2pdf(element, opt);
       FormularioServices.enviarFormulario(opt.filename);
+
+      const x = document.getElementById("sent_email");
+      
+      if (x)
+      {
+        if (x.style.display === 'none')
+        {
+          x.style.display = "block";
+        }
+        else{
+          x.style.display = "none";
+        }
+      }
+     
       console.log('Se ha enviado el pdf');
     },
   },
@@ -300,5 +315,11 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
+#sent_email {
+  width: 100%;
+  padding: 50px 0;
+  text-align: center;
+  background-color: lightblue;
+  margin-top: 20px;
+}
 </style>
