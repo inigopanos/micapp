f5bad04b64d8f607f3d4ce2061372d832c9ce0f0
Iñigo <inigopanos@gmail.com>
2023-09-21 12:54:10 +0200

    Cambiado el post de {data, filename} a una nueva variable que es el objeto de los dos datos.

--- src/router/formulario.service.ts
+++ src/router/formulario.service.ts
@@ -8,8 +8,9 @@ const FORMULARIO_API = 'https://micapp-backend.herokuapp.com/formulario'; // Cam
 // https://micapp-backend.herokuapp.com/formulario
 
 export function enviarFormulario(formulario_pdf: string, filename: string) {
-  console.log('Pre post formulario', formulario_pdf, filename); // Formulario pdf is undefined...
-  return axios.post(`${FORMULARIO_API}`, { data: formulario_pdf, filename }).then((response) => {
+  const data_package = {formulario_pdf, filename};
+  console.log('Pre post formulario', data_package); // Formulario pdf is undefined...
+  return axios.post(`${FORMULARIO_API}`, data_package).then((response) => {
     console.log('Éxito al postear al back: ', response);
   }).catch((error) => {
     console.log('Error al postear al back:', error);
